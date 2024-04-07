daea277340184d3a665d1ba7d49b11867304bf46
Devansh Mittal <edwar@MacBook-Air-2.local>
2024-04-07 20:24:19 +0530

    Initial commit

---
+++ public/src/components/Logout.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { BiPowerOff } from "react-icons/bi";
+import { IoIosVideocam } from "react-icons/io";
+import styled from "styled-components";
+import axios from "axios";
+import { logoutRoute } from "../utils/APIRoutes";
+export default function Logout() {
+  const navigate = useNavigate();
+  const handleClick = async() =>{
+    localStorage.clear();
+    window.location.reload();
+    // navigate('/')
+}
+  const handleVideoCall = async() => {
+    
+  }
+  
+  return (
+    <>
+      <Button onClick={handleClick}>
+        <BiPowerOff />
+      </Button>
+      <Button onClick={handleVideoCall}>
+        <IoIosVideocam />
+      </Button>
+    </>
+  );
+}
+
+const Button = styled.button`
+  display: flex;
+  justify-content: ;
+  align-items: center;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  background-color: #9a86f3;
+  border: none;
+  cursor: pointer;
+  svg {
+    font-size: 1.3rem;
+    color: #ebe7ff;
+  }
+`;
